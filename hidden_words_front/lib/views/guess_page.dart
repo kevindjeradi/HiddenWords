094c059e25ce7b851fc095a72b99c453d9fab519
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-18 20:22:10 +0100

    added eye to reveal text for debug purpose, saving datas in local storage to make sure the data is saved if the user exit, added refresh button that get a new random article, added text verifications for punctuations etc.

--- hidden_words_front/lib/views/guess_page.dart
+++ hidden_words_front/lib/views/guess_page.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hidden_words_front/helpers/logger.dart';
 import 'package:hidden_words_front/logic/world_analyzer.dart';
+import 'package:hidden_words_front/services/article_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GuessPage extends StatefulWidget {
   const GuessPage({super.key});
@@ -11,9 +16,86 @@ class GuessPage extends StatefulWidget {
 class GuessPageState extends State<GuessPage> {
   TextEditingController inputWordController = TextEditingController();
   final WordAnalyzer wordAnalyzer = WordAnalyzer();
-  String article = "Texte de base pour tester, banane bananier";
+  String title = 'Titre';
+  String content = "Texte de base pour tester, banane bananier";
   Set<String> revealedWords = <String>{};
   Map<String, String> bestGuesses = {};
+  ArticleService articleService = ArticleService();
+  bool isTextVisible = false;
+  bool loading = false;
+
+  Future<void> saveArticle(String title, String content) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('article_title', title);
+    await prefs.setString('article_content', content);
+    await prefs.setStringList('revealed_words', revealedWords.toList());
+    await prefs.setString('best_guesses', json.encode(bestGuesses));
+  }
+
+  Future<void> loadArticle() async {
+    setState(() {
+      loading = true;
+    });
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String loadedTitle = prefs.getString('article_title') ?? 'Titre';
+    String loadedContent = prefs.getString('article_content') ??
+        'Texte de base pour tester, banane bananier';
+    List<String> loadedRevealedWords =
+        prefs.getStringList('revealed_words') ?? [];
+    String? loadedBestGuessesStr = prefs.getString('best_guesses');
+    Map<String, String> loadedBestGuesses = loadedBestGuessesStr != null
+        ? Map<String, String>.from(json.decode(loadedBestGuessesStr))
+        : {};
+
+    Log.logger.i("Title: $loadedTitle");
+    Log.logger.i("Content: $loadedContent");
+
+    setState(() {
+      title = loadedTitle;
+      content = loadedContent;
+      revealedWords = loadedRevealedWords.toSet();
+      bestGuesses = loadedBestGuesses;
+      loading = false;
+    });
+  }
+
+  Future<void> getNewArticle() async {
+    try {
+      setState(() {
+        loading = true;
+      });
+      var articleData = await articleService.fetchRandomWikipediaArticle();
+
+      if (articleData != null) {
+        // Reset revealedWords and bestGuesses for the new article
+        revealedWords.clear();
+        bestGuesses.clear();
+
+        // Update local storage with the cleared state
+        await saveArticle('', '');
+
+        setState(() {
+          title = articleData['title'];
+          content = articleData['contentToShow'];
+        });
+
+        // Now save the new article data to local storage
+        await saveArticle(title, content);
+
+        setState(() {
+          loading = false;
+        });
+      }
+    } catch (e) {
+      Log.logger.e("Error fetching article: $e");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadArticle();
+  }
 
   @override
   void dispose() {
@@ -23,64 +105,127 @@ class GuessPageState extends State<GuessPage> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> words = article.split(' ');
+    List<String> lines = content.split('\n');
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hidden Words'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Wrap(
-                spacing: 8.0,
-                runSpacing: 4.0,
-                children: words.map((word) {
-                  String displayWord = revealedWords.contains(word)
-                      ? word
-                      : bestGuesses[word] ?? ' ' * word.length;
-                  bool isBestGuess = bestGuesses.containsKey(word);
-
-                  return Container(
-                    padding: const EdgeInsets.all(8.0),
-                    margin: const EdgeInsets.symmetric(vertical: 2.0),
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    child: Text(
-                      displayWord,
-                      style: TextStyle(
-                        fontStyle:
-                            isBestGuess ? FontStyle.italic : FontStyle.normal,
-                        color: isBestGuess ? Colors.red : Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Center(
+            child: loading
+                ? const CircularProgressIndicator()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            title,
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isTextVisible = !isTextVisible;
+                              });
+                            },
+                            icon: Icon(isTextVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                          ),
+                        ],
                       ),
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 100,
-              child: TextField(
-                controller: inputWordController,
-                decoration: const InputDecoration(
-                  hintText: "Entrez un mot",
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                String inputWord = inputWordController.text;
-                wordAnalyzer.findSimilarWords(inputWord, article, revealWord);
-              },
-              child: const Text("Chercher"),
-            ),
-          ],
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: lines.map((line) {
+                            List<String> words = line.split(' ');
+                            return Wrap(
+                              spacing: 8.0,
+                              runSpacing: 4.0,
+                              children: words.map((word) {
+                                if (word.isEmpty) {
+                                  return const SizedBox.shrink();
+                                }
+                                // Check if the word is a punctuation mark
+                                if (isPunctuation(word)) {
+                                  return _buildWordContainer(
+                                      word, false, false);
+                                }
+
+                                // Existing logic for other words
+                                String displayWord = isTextVisible
+                                    ? word
+                                    : revealedWords.contains(word)
+                                        ? word
+                                        : bestGuesses[word] ??
+                                            ' ' * word.length;
+                                bool isBestGuess =
+                                    bestGuesses.containsKey(word);
+
+                                return _buildWordContainer(
+                                    displayWord, isBestGuess, true);
+                              }).toList(),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: 100,
+                        child: TextField(
+                          controller: inputWordController,
+                          decoration: const InputDecoration(
+                            hintText: "Entrez un mot",
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          String inputWord = inputWordController.text;
+                          wordAnalyzer.findSimilarWords(
+                              inputWord, content, revealWord);
+                        },
+                        child: const Text("Chercher"),
+                      ),
+                    ],
+                  ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getNewArticle,
+        tooltip: 'Changer d\'article',
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+
+  bool isPunctuation(String word) {
+    // A simple check for common punctuation marks
+    return "!.,;:'\"?()-".contains(word);
+  }
+
+  Widget _buildWordContainer(String word, bool isBestGuess, bool isObfuscated) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.symmetric(vertical: 2.0),
+      decoration: BoxDecoration(
+        color: Colors.blue.shade100,
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Text(
+        word,
+        style: TextStyle(
+          fontStyle:
+              isObfuscated && isBestGuess ? FontStyle.italic : FontStyle.normal,
+          color: isObfuscated && isBestGuess ? Colors.red : Colors.black,
         ),
       ),
     );
@@ -91,7 +236,6 @@ class GuessPageState extends State<GuessPage> {
       if (revealedWords.contains(word)) {
         return;
       }
-
       if (similarity == 1) {
         revealedWords.add(word);
         bestGuesses.remove(word);
