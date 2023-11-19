2d386c2cbc0bb703d7d1288df051718a40fb6667
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-19 17:54:32 +0100

    updated article model to have fromJson / toJson / copyWith and toString methods

--- hidden_words_front/lib/models/article.dart
+++ hidden_words_front/lib/models/article.dart
@@ -1,10 +1,13 @@
+// models\article.dart
 class Article {
-  String title;
-  String content;
-  String theme;
-  String url;
-  String difficulty;
-  List<String> hints;
+  final String title;
+  final String content;
+  final String theme;
+  final String url;
+  final String difficulty;
+  final List<String> hints;
+  final Set<String> revealedWords;
+  final Map<String, String> bestGuesses;
 
   Article({
     required this.title,
@@ -13,5 +16,60 @@ class Article {
     this.url = '',
     this.difficulty = '',
     this.hints = const [],
+    this.revealedWords = const {},
+    this.bestGuesses = const {},
   });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'],
+      content: json['content'],
+      theme: json['theme'] ?? '',
+      url: json['url'] ?? '',
+      difficulty: json['difficulty'] ?? '',
+      hints: List<String>.from(json['hints'] ?? []),
+      revealedWords: Set<String>.from(json['revealedWords'] ?? []),
+      bestGuesses: Map<String, String>.from(json['bestGuesses'] ?? {}),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'content': content,
+      'theme': theme,
+      'url': url,
+      'difficulty': difficulty,
+      'hints': hints,
+      'revealedWords': revealedWords.toList(),
+      'bestGuesses': bestGuesses,
+    };
+  }
+
+  Article copyWith({
+    String? title,
+    String? content,
+    String? theme,
+    String? url,
+    String? difficulty,
+    List<String>? hints,
+    Set<String>? revealedWords,
+    Map<String, String>? bestGuesses,
+  }) {
+    return Article(
+      title: title ?? this.title,
+      content: content ?? this.content,
+      theme: theme ?? this.theme,
+      url: url ?? this.url,
+      difficulty: difficulty ?? this.difficulty,
+      hints: hints ?? this.hints,
+      revealedWords: revealedWords ?? this.revealedWords,
+      bestGuesses: bestGuesses ?? this.bestGuesses,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Article(title: $title, content: $content, theme: $theme, url: $url, difficulty: $difficulty, hints: $hints, revealedWords: $revealedWords, bestGuesses: $bestGuesses)';
+  }
 }
