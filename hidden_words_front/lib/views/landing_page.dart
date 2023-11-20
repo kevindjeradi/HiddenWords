39d47b7b3dbfc9ee38bf9835b70c1915d6fe7d64
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-20 23:24:14 +0100

    adding admin button in landing page

--- hidden_words_front/lib/views/landing_page.dart
+++ hidden_words_front/lib/views/landing_page.dart
@@ -1,5 +1,6 @@
 // views/landing_page.dart
 import 'package:flutter/material.dart';
+import 'package:hidden_words_front/views/admin.dart';
 import 'package:hidden_words_front/views/infernal_mode.dart';
 
 class LandingPage extends StatefulWidget {
@@ -13,65 +14,80 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Hidden Words'),
+      appBar: AppBar(
+        title: const Text('Hidden Words'),
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            const Text(
+              'Bienvenue sur Hidden Words !',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Text(
+                  'Modes de jeu',
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const InfernalMode(),
+                            ),
+                          );
+                        },
+                        child: const Text('Mode infernal'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.grey[400])),
+                        onPressed: () {},
+                        child: const Text('Mode normal (à venir)'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.grey[400])),
+                        onPressed: () {},
+                        child: const Text('Mode à thème (à venir)'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Admin(),
+            ),
+          );
+        },
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Icon(Icons.admin_panel_settings_sharp), Text("Admin")],
         ),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              const Text(
-                'Bienvenue sur Hidden Words !',
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  const Text(
-                    'Modes de jeu',
-                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 16),
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const InfernalMode(),
-                              ),
-                            );
-                          },
-                          child: const Text('Mode infernal'),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.grey[400])),
-                          onPressed: () {},
-                          child: const Text('Mode normal (à venir)'),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.grey[400])),
-                          onPressed: () {},
-                          child: const Text('Mode à thème (à venir)'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ])));
+      ),
+    );
   }
 }
