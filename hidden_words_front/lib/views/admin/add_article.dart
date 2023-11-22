bb78e50c4c93533cdbebea368bc926d92e5decbf
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-23 00:15:13 +0100

    updated add_article to use the new ArticleDetail

--- hidden_words_front/lib/views/admin/add_article.dart
+++ hidden_words_front/lib/views/admin/add_article.dart
@@ -1,10 +1,9 @@
+// add_article.dart
 import 'package:flutter/material.dart';
 import 'package:hidden_words_front/helpers/logger.dart';
-import 'package:hidden_words_front/logic/difficulties.dart';
-import 'package:hidden_words_front/logic/themes.dart';
 import 'package:hidden_words_front/models/article.dart';
 import 'package:hidden_words_front/services/article_service.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:hidden_words_front/views/admin/article_detail.dart';
 
 class AddArticle extends StatefulWidget {
   const AddArticle({super.key});
@@ -16,18 +15,12 @@ class AddArticle extends StatefulWidget {
 class AddArticleState extends State<AddArticle> {
   Article? currentArticle;
   final ArticleService articleService = ArticleService();
-  String? selectedDifficulty;
-  String? selectedTheme;
-  List<TextEditingController> hintControllers = [TextEditingController()];
-  final TextEditingController contentController = TextEditingController();
-  bool isContentEditable = false;
   bool loading = false;
 
   void fetchArticle() async {
     setState(() {
       loading = true;
     });
-    resetForm();
     var articleData = await articleService.fetchRandomWikipediaArticle();
     if (articleData != null) {
       setState(() {
@@ -41,7 +34,6 @@ class AddArticleState extends State<AddArticle> {
             hints: [],
             revealedWords: {},
             bestGuesses: {});
-        contentController.text = currentArticle!.content;
         Log.logger.i(currentArticle.toString());
         setState(() {
           loading = false;
@@ -50,71 +42,8 @@ class AddArticleState extends State<AddArticle> {
     }
   }
 
-  void _launchURL(String urlString) async {
-    final Uri url = Uri.parse(urlString);
-    if (!await launchUrl(url)) {
-      throw Exception('Could not launch $url');
-    }
-  }
-
-  void toggleContentEditMode() {
-    setState(() {
-      isContentEditable = !isContentEditable;
-    });
-  }
-
-  void addHintField() {
-    setState(() {
-      hintControllers.add(TextEditingController());
-    });
-  }
-
-  void removeHintField(int index) {
-    if (hintControllers.length > 1) {
-      setState(() {
-        hintControllers.removeAt(index);
-      });
-    }
-  }
-
-  void resetForm() {
-    contentController.clear();
-    hintControllers.clear();
-    hintControllers.add(TextEditingController());
-    selectedTheme = null;
-    selectedDifficulty = null;
-  }
-
-  void saveArticle() async {
-    setState(() {
-      loading = true;
-    });
-    if (currentArticle != null) {
-      try {
-        currentArticle = currentArticle!.copyWith(
-          theme: selectedTheme,
-          difficulty: selectedDifficulty,
-          content: contentController.text,
-          hints: hintControllers.map((controller) => controller.text).toList(),
-        );
-        await articleService.createArticle(currentArticle!);
-        Log.logger.i("article sauvegardé");
-        resetForm();
-        fetchArticle();
-      } catch (e) {
-        Log.logger.i("article pas sauvegardé");
-      } finally {
-        setState(() {
-          loading = false;
-        });
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    List<String> contentLines = currentArticle?.content.split('\n') ?? [];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Créer un article'),
@@ -130,192 +59,7 @@ class AddArticleState extends State<AddArticle> {
                 )
           : loading
               ? const Center(child: CircularProgressIndicator())
-              : SingleChildScrollView(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      ElevatedButton(
-                        onPressed: fetchArticle,
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Theme.of(context).primaryColor),
-                        child: const Text('Changer d\'article'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(currentArticle!.title,
-                            style: Theme.of(context).textTheme.titleLarge),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: GestureDetector(
-                          onTap: () => _launchURL(currentArticle!.url),
-                          child: Text(
-                            currentArticle!.url,
-                            style: const TextStyle(
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: GestureDetector(
-                          onTap: showEditContentDialog,
-                          child: Container(
-                            height: 350,
-                            padding: const EdgeInsets.all(8.0),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey),
-                              borderRadius: BorderRadius.circular(5.0),
-                            ),
-                            child: SingleChildScrollView(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: contentLines.map((line) {
-                                  bool isTitle = line.startsWith('{{{') &&
-                                      line.endsWith('}}}');
-                                  String displayText = isTitle
-                                      ? line.substring(3, line.length - 3)
-                                      : line;
-                                  return Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: isTitle ? 8.0 : 4.0),
-                                    child: Text(
-                                      displayText,
-                                      style: TextStyle(
-                                        fontWeight: isTitle
-                                            ? FontWeight.bold
-                                            : FontWeight.normal,
-                                      ),
-                                    ),
-                                  );
-                                }).toList(),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: Center(
-                              child: DropdownButton<String>(
-                                isExpanded: true,
-                                value: selectedTheme,
-                                hint: const Text('Thème'),
-                                onChanged: (String? newValue) {
-                                  setState(() {
-                                    selectedTheme = newValue;
-                                  });
-                                },
-                                items: articlesThemes
-                                    .map<DropdownMenuItem<String>>(
-                                        (String value) {
-                                  return DropdownMenuItem<String>(
-                                    value: value,
-                                    child: Text(value),
-                                  );
-                                }).toList(),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Center(
-                              child: DropdownButton<String>(
-                                value: selectedDifficulty,
-                                hint: const Text('Difficulté'),
-                                onChanged: (String? newValue) {
-                                  setState(() {
-                                    selectedDifficulty = newValue;
-                                  });
-                                },
-                                items: articlesDifficulties
-                                    .map<DropdownMenuItem<String>>(
-                                        (String value) {
-                                  return DropdownMenuItem<String>(
-                                    value: value,
-                                    child: Text(value),
-                                  );
-                                }).toList(),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      ...List.generate(hintControllers.length, (index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: TextField(
-                                  controller: hintControllers[index],
-                                  decoration: InputDecoration(
-                                    hintText: 'Indice ${index + 1}',
-                                  ),
-                                ),
-                              ),
-                              if (hintControllers.length > 1)
-                                IconButton(
-                                  icon: const Icon(Icons.remove),
-                                  onPressed: () => removeHintField(index),
-                                ),
-                              if (index == hintControllers.length - 1)
-                                IconButton(
-                                  icon: const Icon(Icons.add),
-                                  onPressed: addHintField,
-                                ),
-                            ],
-                          ),
-                        );
-                      }),
-                      ElevatedButton(
-                        onPressed: saveArticle,
-                        child: const Text('Sauvegarder l\'article'),
-                      ),
-                    ],
-                  ),
-                ),
-    );
-  }
-
-  void showEditContentDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        TextEditingController dialogController =
-            TextEditingController(text: currentArticle?.content);
-        return AlertDialog(
-          title: const Text("Editer le contenu"),
-          content: TextField(
-            controller: dialogController,
-            decoration: const InputDecoration(hintText: 'Contenu'),
-            maxLines: null,
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Annuler'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('Sauvegarder'),
-              onPressed: () {
-                setState(() {
-                  contentController.text = dialogController.text;
-                  currentArticle =
-                      currentArticle?.copyWith(content: dialogController.text);
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+              : ArticleDetail(article: currentArticle!),
     );
   }
 }
