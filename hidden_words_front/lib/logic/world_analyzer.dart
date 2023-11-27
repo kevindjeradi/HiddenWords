a0b2060558c2a21020df612b031c8d34085d266d
kevin <kevin-hamza.djeradi@epitech.eu>
2023-11-27 21:54:51 +0100

    fixed removePunctuation to not remoee accented characters

--- hidden_words_front/lib/logic/world_analyzer.dart
+++ hidden_words_front/lib/logic/world_analyzer.dart
@@ -1,10 +1,11 @@
 // logic/world_analyzer.dart
+import 'package:characters/characters.dart';
 import 'package:hidden_words_front/helpers/logger.dart';
 import 'package:text_analysis/text_analysis.dart';
 
 class WordAnalyzer {
   String removePunctuation(String text) {
-    return text.replaceAll(RegExp(r'[^\w\s]', unicode: true), '');
+    return text.replaceAll(RegExp(r'(?<=\w)[.,:;]+'), '');
   }
 
   double getSimilarity(String? guess, String word) {
@@ -14,44 +15,123 @@ class WordAnalyzer {
 
   String normalize(String word) {
     Map<String, String> diacriticsMap = {
-      'à': 'a', 'á': 'a', 'â': 'a', 'ä': 'a', // Add more mappings as needed
-      // ... other diacritic mappings ...
+      'à': 'a',
+      'á': 'a',
+      'â': 'a',
+      'ä': 'a',
+      'ã': 'a',
+      'å': 'a',
+      'ā': 'a',
+      'ç': 'c',
+      'ć': 'c',
+      'č': 'c',
+      'è': 'e',
+      'é': 'e',
+      'ê': 'e',
+      'ë': 'e',
+      'ē': 'e',
+      'ė': 'e',
+      'ę': 'e',
+      'ì': 'i',
+      'í': 'i',
+      'î': 'i',
+      'ï': 'i',
+      'ī': 'i',
+      'į': 'i',
+      'ı': 'i',
+      'ł': 'l',
+      'ñ': 'n',
+      'ń': 'n',
+      'ň': 'n',
+      'ò': 'o',
+      'ó': 'o',
+      'ô': 'o',
+      'ö': 'o',
+      'õ': 'o',
+      'ø': 'o',
+      'ō': 'o',
+      'ù': 'u',
+      'ú': 'u',
+      'û': 'u',
+      'ü': 'u',
+      'ū': 'u',
+      'ÿ': 'y',
+      'ź': 'z',
+      'ż': 'z',
+      'ž': 'z',
+      'À': 'A',
+      'Á': 'A',
+      'Â': 'A',
+      'Ä': 'A',
+      'Ã': 'A',
+      'Å': 'A',
+      'Ā': 'A',
+      'Ç': 'C',
+      'Ć': 'C',
+      'Č': 'C',
+      'È': 'E',
+      'É': 'E',
+      'Ê': 'E',
+      'Ë': 'E',
+      'Ē': 'E',
+      'Ė': 'E',
+      'Ę': 'E',
+      'Ì': 'I',
+      'Í': 'I',
+      'Î': 'I',
+      'Ï': 'I',
+      'Ī': 'I',
+      'Į': 'I',
+      'I': 'I',
+      'Ł': 'L',
+      'Ñ': 'N',
+      'Ń': 'N',
+      'Ň': 'N',
+      'Ò': 'O',
+      'Ó': 'O',
+      'Ô': 'O',
+      'Ö': 'O',
+      'Õ': 'O',
+      'Ø': 'O',
+      'Ō': 'O',
+      'Ù': 'U',
+      'Ú': 'U',
+      'Û': 'U',
+      'Ü': 'U',
+      'Ū': 'U',
+      'Ÿ': 'Y',
+      'Ź': 'Z',
+      'Ż': 'Z',
+      'Ž': 'Z',
     };
 
-    String normalized = word
-        .split('')
+    // Using the characters package to handle grapheme clusters
+    String normalized = word.characters
         .map((c) {
           String normalizedChar = diacriticsMap[c] ?? c;
-          Log.logger.i("Character: $c, Normalized: $normalizedChar");
           return normalizedChar;
         })
         .join()
         .toLowerCase();
 
-    Log.logger.i("Original: $word, Normalized: $normalized");
     return normalized;
   }
 
   void findSimilarWords(
       String inputWord, String inputText, Function revealWordCallback) {
-    String cleanedInputWord = removePunctuation(inputWord);
-    String normalizedInputWord = normalize(cleanedInputWord);
+    String normalizedInputWord = normalize(removePunctuation(inputWord));
 
     List<String> wordsInText = inputText.split(' ');
 
     for (String word in wordsInText) {
-      String cleanedWord = removePunctuation(word);
-      String normalizedWord = normalize(cleanedWord);
-
-      Log.logger
-          .i("Normalized Words: $normalizedInputWord <-> $normalizedWord");
+      String normalizedWord = normalize(removePunctuation(word));
 
       TermSimilarity similarity =
           TermSimilarity(normalizedInputWord, normalizedWord);
 
       if (similarity.characterSimilarity >= 0.65) {
         Log.logger.i(
-            "Similarité de zinzin: $inputWord <-> $word (Similarité: ${similarity.characterSimilarity})");
+            "Similarité de zinzin: $normalizedInputWord <-> $normalizedWord (Similarité: ${similarity.characterSimilarity})");
         revealWordCallback(word, inputWord, similarity.characterSimilarity);
       }
     }
